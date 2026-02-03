1c571078f62d327f9375c327e06c767c80a921fa
nhsbsa-rowil <rowil@nhsbsa.nhs.uk>
2026-02-03 15:44:54 +0000

    Created LSF Public V10 using V9, and fixed some routing issues

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,33 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-module.exports = router
+// V10 Public
+router.get('/lsf-public/v10/:page', (req, res) => {
+    const number = 'v10';
+    const templatePath = `lsf-public/v10/${req.params.page}`;
+    res.locals.number = number;
+    res.render(templatePath, { number });
+});
+
+
+
+// ================================
+// Routes for LSF Portal
+// ================================
+
+
+
+// ================================
+// Routes for LSF Public
+// ================================
+router.use('/lsf-public/v10', require('./views/lsf-public/v10/_routes'));
+
+
+
+// ================================
+// Routes for LSF Web
+// ================================
+
+
+
+module.exports = router;
