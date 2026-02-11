d27592ea4cc5a8288cfe000c6462cbcf0cae5531
Charlotte Broad <charlotte.broad@nhsbsa.nhs.uk>
2026-02-11 21:23:26 +0000

    apply journey updates

--- app/views/lsf-public/v10/sign-in/_routes.js
+++ app/views/lsf-public/v10/sign-in/_routes.js
@@ -11,15 +11,23 @@ const router = express.Router();
 
 
 
+router.post('/lsf-public/v10/sign-in/course-study', (req, res) => {
+
+  const course = req.body.course;
+
+  // your course logic here
+  res.redirect('/lsf-public/v10/sign-in/new-employment');
+
+});
 
 router.post('/new-employment', (req, res) => {
 
   const employNew = req.session.data['OptionEmployment']
 
   if (employNew== 'yes') {
-   res.redirect('/course-length');
+   res.redirect('/lsf-public/v10/sign-in/course-length');
   } else {
-    res.redirect('/new-employment-kickout');
+    res.redirect('/lsf-public/v10/sign-in/new-employment-kickout');
   }
 
 })
